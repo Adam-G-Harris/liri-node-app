4e3b43e2f509885eb3f051486b480004d18a32e2
Adam <adamgrantharris14@gmail.com>
2018-03-23 23:49:43 -0500

    Testing node modules

--- script/liri.js
+++ script/liri.js
@@ -2,4 +2,8 @@ require("dotenv").config();
 
 const keys = require('./keys');
 
-console.log(keys);
+const spotify = new Spotify(keys.spotify);
+
+const twitter = new Twitter(keys.twitter);
+
+console.log(twitter);
