1e71c304fd9879ab5a811a6276c0020b1fbd4766
Adam <adamgrantharris14@gmail.com>
2018-03-25 14:45:24 -0500

    Init function & user input validation

--- script/liri.js
+++ script/liri.js
@@ -1,15 +1,107 @@
-require("dotenv").config();
-
-const args = require('minimist')(process.argv.slice(2));
-
+const screenName = require('twitter-screen-name');
+const readline = require('readline');
+const request = require('request');
+const axios = require('axios');
 const keys = require('./keys');
 
-const spotify = keys.spotify;
-const twitter = keys.twitter;
+// Loads .env file variables into process.env
+require("dotenv").config();
 
+// Potential user inputs
 const userArgs = {
     twitter: 'my-tweets',
     spotify: 'spotify-this-song',
     omdb: 'movie-this',
     do: 'do-what-it-says'
 };
+
+// const config = {
+//     headers: {
+
+//     }
+// };
+
+// First function that is executed
+function init() {
+
+    let staticArgs = process.argv.slice(0, 2);
+    let userInputArg = process.argv.slice(2, 3);
+    let additionalArgs = process.argv.slice(3);
+
+    let spotify = keys.spotify;
+    let twitter = keys.twitter;
+
+    // Checks user argument value
+    switch (userInputArg[0]) {
+
+        case userArgs.twitter:
+            getTwitter();
+            break;
+        case userArgs.spotify:
+            getSpotify();
+            break;
+        case userArgs.omdb:
+            getOmdb();
+            break;
+        case userArgs.do:
+            getRandomInfo();
+            break;
+        default:
+            console.log(`Make sure your input is one of the following: "${userArgs.twitter}", "${userArgs.spotify}", "${userArgs.omdb}", "${userArgs.do}".`);
+            break;
+    }
+}
+
+async function getTwitter() {
+
+    let twitterName = await getHandle();
+
+    let url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${twitterName}&count=20`;
+
+    if (twitterName === null) {
+        console.log("Sorry that Twitter name couldn't be found...");
+        return;
+    }
+
+    axios.get(url)
+        .then(function (res) {
+            console.log(res.data)
+        })
+        .catch(function (err) {
+            console.log(err.message)
+        });
+}
+
+function getHandle() {
+
+    let tempTwitterName;
+
+    let read = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise(resolve => {
+        read.question('Please enter your Twitter handle ==> ', (answer) => {
+            readline.moveCursor(read.output, 10, 1);
+            tempTwitterName = screenName(answer);
+            resolve(tempTwitterName);
+            read.close();
+        });
+    });
+}
+
+function getSpotify() {
+
+}
+
+function getOmdb() {
+
+}
+
+function getRandomInfo() {
+
+}
+
+// Application start call
+init();
