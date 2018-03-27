8f338b6a55931b61666ee91518fb7388f9689fca
Adam <adamgrantharris14@gmail.com>
2018-03-27 17:31:42 -0500

    Logic is functional, everything works as planned. TODO: Error tests

--- script/liri.js
+++ script/liri.js
@@ -1,8 +1,11 @@
 const screenName = require('twitter-screen-name');
+const Spotify = require('node-spotify-api');
 const readline = require('readline');
 const request = require('request');
+const col = require('columnify');
 const axios = require('axios');
 const keys = require('./keys');
+const fs = require('fs');
 
 // Loads .env file variables into process.env
 require("dotenv").config();
@@ -15,15 +18,19 @@ const userArgs = {
     do: 'do-what-it-says'
 };
 
+// User interaction interface
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 // First function that is executed
 function init() {
 
-    let staticArgs = process.argv.slice(0, 2);
-    let userInputArg = process.argv.slice(2, 3);
-    let additionalArgs = process.argv.slice(3);
+    space();
 
-    let spotify = keys.spotify;
-    let twitter = keys.twitter;
+    const userInputArg = process.argv.slice(2, 3);
+    const userAdditionalArg = process.argv.slice(3);
 
     // Checks user argument value
     switch (userInputArg[0]) {
@@ -32,52 +39,74 @@ function init() {
             getTwitter();
             break;
         case userArgs.spotify:
-            getSpotify();
+            getSpotify(userAdditionalArg);
             break;
         case userArgs.omdb:
-            getOmdb();
+            getOmdb(userAdditionalArg);
             break;
         case userArgs.do:
-            getRandomInfo();
+            getFileInfo();
             break;
         default:
-            console.log(`Hey ${process.env['USERNAME']}, make sure your input is one of the following: "${userArgs.twitter}", "${userArgs.spotify}", "${userArgs.omdb}", "${userArgs.do}".`);
+            getInputError();
             break;
     }
 }
 
+// Application start call
+init();
+
+// Twitter call
 async function getTwitter() {
 
-    let twitterName = await getHandle();
+    const twitterName = await getHandle();
 
-    let url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${twitterName}&count=20`;
+    const url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${twitterName}&trim_user=true&count=20`;
 
     if (twitterName === null) {
-        console.log("Sorry that Twitter name couldn't be found...");
+        space();
+        console.log(`> Sorry ${process.env['USERNAME']}... I couldn't find that Twitter name.`);
+        space();
         return;
     }
 
-    axios.get(url)
-        .then(function (res) {
-            console.log(res.data)
-        })
-        .catch(function (err) {
-            console.log(err.message)
-        });
+    const options = {
+        method: 'GET',
+        url: url,
+        qs: {
+            "screen_name": twitterName
+        },
+        json: true,
+        headers: {
+            "Authorization": "Bearer " + process.env['TWITTER_BEARER_TOKEN']
+        }
+    };
+
+    request(options, function (error, response, body) {
+
+        for (let i = 0; i < body.length; i++) {
+
+            if (i === 0) {
+                separate();
+            }
+            console.log(body[i].created_at, ' - ', body[i].text);
+            if (i === body.length - 1) {
+                separate();
+                console.log(`> These are ${twitterName}'s 20 most recent tweets.`);
+                space();
+            }
+        }
+    });
 }
 
+// Gets Twitter's screen name regardless of format
 function getHandle() {
 
     let tempTwitterName;
 
-    let read = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
     return new Promise(resolve => {
-        read.question(`Hey ${process.env['USERNAME']}, enter the name of any Twitter account ==> `, (answer) => {
-            readline.moveCursor(read.output, 10, 1);
+
+        read.question(`> Hey ${process.env['USERNAME']}, enter the name of any Twitter account > `, (answer) => {
             tempTwitterName = screenName(answer);
             resolve(tempTwitterName);
             read.close();
@@ -85,17 +114,135 @@ function getHandle() {
     });
 }
 
-function getSpotify() {
+// Spotify call
+function getSpotify(song) {
 
+    if (song.length < 1) {
+        song = ['The Sign, Ace of Base'];
+    }
+
+    const spotify = new Spotify({
+        id: process.env['SPOTIFY_ID'],
+        secret: process.env['SPOTIFY_SECRET']
+    });
+
+    spotify
+        .search({
+            type: 'track',
+            query: song,
+            limit: 1
+        })
+        .then(res => handleSpotify(res, song))
+        .catch(err => console.log(err))
 }
 
-function getOmdb() {
+// Spotify handler & output
+function handleSpotify(res, song) {
 
+    const narrow = res.tracks.items[0];
+
+    const narrowObj = {
+        "Artist": narrow.album.artists[0].name,
+        "Song": narrow.name,
+        "Link": narrow.album.artists[0].external_urls.spotify,
+        "Album": narrow.album.name
+    };
+
+    console.log(`> Hey ${process.env['USERNAME']}, you searched for - ${song}.`);
+    separate();
+
+    console.log(col(narrowObj, {
+        truncate: true,
+        showHeaders: false,
+        columnSplitter: '  -  '
+    }));
+
+    separate();
+    console.log("> This is Spotify's response to your input.");
+    space();
+    read.close();
 }
 
-function getRandomInfo() {
+// OMDB call
+function getOmdb(movie) {
 
+    let parsedBody, info;
+
+    if (movie.length < 1) {
+        movie = ['Mr. Nobody'];
+    }
+
+    const url = `http://www.omdbapi.com/?&type=movie&t=${movie}&apikey=${process.env['OMDB_KEY']}`;
+
+    request(url, function (error, response, body) {
+        parsedBody = JSON.parse(body);
+        info = {
+            "Title": parsedBody.Title,
+            "Release Year": parsedBody.Year,
+            "IMDB Rating": parsedBody.imdbRating,
+            "Rotten Tomatoes": parsedBody.Ratings[1].Value,
+            "Origin Country": parsedBody.Country,
+            "Language": parsedBody.Language,
+            "Plot": parsedBody.Plot,
+            "Actors": parsedBody.Actors,
+        };
+        console.log(`> Hey ${process.env['USERNAME']}, you searched for - ${movie}.`);
+        separate();
+        console.log(col(info, {
+            truncate: true,
+            showHeaders: false,
+            columnSplitter: '  -  '
+        }));
+        separate();
+        console.log("> This is OMDB's response to your input.");
+        space();
+    });
+    read.close();
 }
 
-// Application start call
-init();
+// Grabs random file's contents
+function getFileInfo() {
+
+    fs.readFile('random.txt', 'utf8', (err, content) => {
+
+        let fileCommand = content.split('"').splice(0, 1).toString().trim();
+        let additionalCommand = content.split('"').splice(1, 1).toString().trim();
+
+        // Checks user argument value
+        switch (fileCommand) {
+
+            case userArgs.twitter:
+                getTwitter();
+                break;
+            case userArgs.spotify:
+                getSpotify(additionalCommand);
+                break;
+            case userArgs.omdb:
+                getOmdb(additionalCommand);
+                break;
+            case userArgs.do:
+                getFileInfo();
+                break;
+            default:
+                getInputError();
+                break;
+        }
+    });
+}
+
+// Incorrect user input handler
+function getInputError() {
+    console.log(`> Hey ${process.env['USERNAME']}, make sure your input is one of the following: "${userArgs.twitter}", "${userArgs.spotify}", "${userArgs.omdb}", "${userArgs.do}".`);
+    space();
+    read.close();
+}
+
+// Console formatting
+function separate() {
+    console.log('-'.repeat(process.stdout.columns));
+}
+
+// Console formatting
+function space() {
+    console.log('');
+}
